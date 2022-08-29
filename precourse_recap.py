52924f83ca7f7f4eeb1cdac5544dd130ac9447a3
Frensis Bajrami <frensisbajrami@googlemail.com>
2022-08-29 17:14:13 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,20 @@
+number_1 = 10
+number_2 = 12
+
+print( number_1 + number_2)
+
+number_3 = 400
+number_4 = 200
+
+print (number_3 - number_4)
+
+number_5 = 2000
+number_6 = 2
+
+print (number_5 / number_6)
+
+print (number_5 * number_6)
+
+
+
+
